295f8e581ac05cadffe0b75418825cdd7f41b24a
Andrei Makhnach <ahdpyxa88@gmail.com>
2023-10-02 01:54:09 +0300

    feat: add solution

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,20 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.mid = this.max - Math.floor((this.max - this.min)/2);
+        return this.mid;
     }
 
     lower() {
-
+        this.max = this.mid;
     }
 
     greater() {
-
+        this.min = this.mid;
     }
 }
+
+
 
 module.exports = GuessingGame;
